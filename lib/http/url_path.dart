8e6495ec24e673a7e9182d53cbdae1db4113160d
ikunhuaji <1550309783@qq.com>
2024-10-31 20:46:31 +0800

    新增接口链接存储

---
+++ lib/http/url_path.dart
@@ -0,0 +1,7 @@
+class UrlPath {
+  static const String BaseUrl = "http://192.168.242.236:8080";
+
+  static const String captchaImageUrl="/code/image";
+
+  static const String loginUrl="/userLogin";
+}
