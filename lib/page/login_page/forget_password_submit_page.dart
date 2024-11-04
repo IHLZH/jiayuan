5c6c8b9cd9b7abab19c1c70bd2d1e87ac1ebbd72
ikunhuaji <1550309783@qq.com>
2024-11-04 20:23:18 +0800

    优化代码

--- lib/page/login_page/forget_password_submit_page.dart
+++ lib/page/login_page/forget_password_submit_page.dart
@@ -10,15 +10,19 @@ class ForgetPasswordSubmitPage extends StatefulWidget {
   final String input;
   final bool isEmail;
 
-  const ForgetPasswordSubmitPage({Key? key, required this.input, required this.isEmail}) : super(key: key);
+  const ForgetPasswordSubmitPage(
+      {Key? key, required this.input, required this.isEmail})
+      : super(key: key);
 
   @override
-  _ForgetPasswordSubmitPageState createState() => _ForgetPasswordSubmitPageState();
+  _ForgetPasswordSubmitPageState createState() =>
+      _ForgetPasswordSubmitPageState();
 }
 
 class _ForgetPasswordSubmitPageState extends State<ForgetPasswordSubmitPage> {
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
   final FocusNode _passwordFocusNode = FocusNode();
   final FocusNode _confirmPasswordFocusNode = FocusNode();
 
@@ -46,19 +50,20 @@ class _ForgetPasswordSubmitPageState extends State<ForgetPasswordSubmitPage> {
       return;
     }
 
-    String url = UrlPath.resetPasswordUrl+"?countId=${widget.input}&password=$password&confirmPassword=$confirmPassword";
+    String url = UrlPath.resetPasswordUrl +
+        "?countId=${widget.input}&password=$password&confirmPassword=$confirmPassword";
 
     try {
       final response = await DioInstance.instance().post(path: url);
 
-      if(response.statusCode==200){
-        if(response.data['code']==200){
-          showToast(response.data['message'],duration: Duration(seconds: 1));
+      if (response.statusCode == 200) {
+        if (response.data['code'] == 200) {
+          showToast(response.data['message'], duration: Duration(seconds: 1));
           RouteUtils.pushNamedAndRemoveUntil(context, RoutePath.loginPage);
-        }else{
+        } else {
           showToast(response.data['message'], duration: Duration(seconds: 1));
         }
-      }else{
+      } else {
         showToast("连接不到服务器", duration: Duration(seconds: 1));
       }
     } catch (e) {
@@ -130,10 +135,12 @@ class _ForgetPasswordSubmitPageState extends State<ForgetPasswordSubmitPage> {
               SizedBox(height: 20.h),
               ElevatedButton(
                 onPressed: _resetPassword,
-                child: Text('更改密码', style: TextStyle(fontSize: 18, color: Colors.white)),
+                child: Text('更改密码',
+                    style: TextStyle(fontSize: 18, color: Colors.white)),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Theme.of(context).primaryColor,
-                  padding: EdgeInsets.symmetric(horizontal: 50.w, vertical: 15.h),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 50.w, vertical: 15.h),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10.r),
                   ),
