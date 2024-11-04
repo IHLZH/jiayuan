90c5e62c44640f7c03659c4b6e9445b4e0fd42c3
ikunhuaji <1550309783@qq.com>
2024-11-04 17:21:11 +0800

    添加注册页

---
+++ lib/page/register_page/register_password_submit_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class RegisterPasswordSubmitPage extends StatefulWidget{
+  const RegisterPasswordSubmitPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _RegisterPasswordSubmitPageState();
+
+}
+
+class _RegisterPasswordSubmitPageState extends State<RegisterPasswordSubmitPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
