6e89fe688775476b555df7473139f24d39639257
ikunhuaji <1550309783@qq.com>
2024-11-04 16:43:28 +0800

    添加更改密码获取验证码接口和修改密码接口

--- lib/http/url_path.dart
+++ lib/http/url_path.dart
@@ -1,13 +1,27 @@
 class UrlPath {
   static const String BaseUrl = "http://192.168.242.236:8080";
 
+  //账号登录获取图像验证码接口
   static const String captchaImageUrl="/code/image";
 
+  //账号登录接口
   static const String loginUrl="/login/loginBoth";
 
-  static const String getEmailCodeUrl="/email/login";
+  //邮箱获取验证码接口
+  static const String getEmailCodeUrl="/sendEmailCode";
 
+  //邮箱登录接口
   static const String loginWithEmailCodeUrl = "/login/email";
 
-  static const String forgetPasswordCheckCodeUrl="/password/resetPassword";
+  //手机获取验证码接口
+  static const String getPhoneCodeUrl="/sendSmsCode";
+
+  //邮箱忘记密码检验验证码接口
+  static const String checkPasswordEmailCodeUrl="/password/email";
+
+  //手机忘记密码检验验证码接口
+  static const String checkPasswordPhoneCodeUrl="/password/phone";
+
+  //重置密码接口
+  static const String resetPasswordUrl="/password/resetPassword";
 }
