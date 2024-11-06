e2e09cb967b67b9fd3eba351de224a23b9459b7d
ywl <3101653644@qq.com>
2024-11-06 08:42:30 +0800

    修复输入框占有焦点，导致软键盘弹出的问题

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -1,8 +1,14 @@
+import 'package:jiayuan/repository/model/StandardPrice.dart';
 import 'package:jiayuan/repository/model/user.dart';
 
 class Global{
+
   static User? userInfo;
   static String? token;
   static String? input;
   static String? password;
+
+  // 价格标准
+  static List<StandardPrice>? standPrices;
+
 }
