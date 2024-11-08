3a2d65c6c12f832ae868c4ba57d5e0a6a35d1a07
ikunhuaji <1550309783@qq.com>
2024-11-08 16:47:09 +0800

    新增个人信息修改页

---
+++ lib/page/user_page/profile_edit_page/profile_edit_page.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:jiayuan/repository/model/user.dart';
+import 'package:jiayuan/route/route_utils.dart';
+import 'package:jiayuan/utils/constants.dart';
+import 'package:jiayuan/utils/global.dart';
+
+bool isProduction = Constants.IS_Production;
+
+class ProfileEditPage extends StatefulWidget {
+  const ProfileEditPage({Key? key}) : super(key: key);
+  @override
+  State<ProfileEditPage> createState() => _ProfileEditPageState();
+}
+
+class _ProfileEditPageState extends State<ProfileEditPage> {
+  // 控制器和状态变量声明
+  final TextEditingController _nickNameController = TextEditingController();
+  int _selectedSex = 0;
+  String? _avatarUrl;
+
+  // 添加最大字符限制
+  static const int _maxNickNameLength = 20;
+
+  // 添加一个状态变量来跟踪当前输入长度
+  int _currentLength = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _nickNameController.text = Global.userInfo?.nickName ?? '';
+    _selectedSex = Global.userInfo?.userSex ?? 0;
+    _avatarUrl = Global.userInfo?.userAvatar;
+
+    // 添加文本变化监听器
+    _currentLength = _nickNameController.text.length;
+    _nickNameController.addListener(() {
+      setState(() {
+        _currentLength = _nickNameController.text.length;
+      });
+    });
+  }
+
+  void _updateUserInfo() {
+    //TODO: 后端更新用户信息
+  }
+
+  void _selectAvatar() {
+    // TODO: 实现头像选择逻辑
+  }
+
+  void _saveChanges() {
+    // 创建一个新的 User 对象并更新
+    User updatedUser = User(
+      userId: Global.userInfo?.userId ?? 0,
+      userName: Global.userInfo?.userName ?? '',
+      nickName: _nickNameController.text,
+      userPassword: Global.userInfo?.userPassword ?? '',
+      userAvatar: _avatarUrl ?? '默认头像',
+      userSex: _selectedSex,
+      userPhoneNumber: Global.userInfo?.userPhoneNumber ?? '',
+      createdTime: Global.userInfo?.createdTime ?? '',
+      updatedTime: Global.userInfo?.updatedTime ?? '',
+      loginIp: Global.userInfo?.loginIp ?? '',
+      loginTime: Global.userInfo?.loginTime ?? '',
+      userType: Global.userInfo?.userType ?? 0,
+      userStatus: Global.userInfo?.userStatus ?? 0,
+    );
+
+    // 更新 userInfoNotifier 以通知监听者
+    Global.userInfoNotifier.value = updatedUser;
+
+    //更新后端数据
+    _updateUserInfo();
+
+    // 保存修改
+    RouteUtils.pop(context);
+  }
+
+  @override
+  void dispose() {
+    _nickNameController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('个人资料编辑'),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          // 头像选择
+          Center(
+            child: GestureDetector(
+              onTap: _selectAvatar,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: _avatarUrl != null && _avatarUrl != '默认头像'
+                    ? NetworkImage(_avatarUrl!)
+                    : null,
+                child: _avatarUrl == null || _avatarUrl == '默认头像'
+                    ? Icon(Icons.person, size: 50)
+                    : null, // 如果头像为空，显示默认头像
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+
+          // 修改昵称输入框部分
+          TextField(
+            controller: _nickNameController,
+            maxLength: _maxNickNameLength, // 添加最大长度限制
+            decoration: InputDecoration(
+              labelText: '昵称',
+              border: OutlineInputBorder(),
+              counterText:
+                  '$_currentLength/$_maxNickNameLength', // 使用状态变量显示当前长度
+            ),
+          ),
+          SizedBox(height: 20),
+
+          // 性别选择
+          Card(
+            color: Colors.white,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('性别', style: TextStyle(fontSize: 16)),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RadioListTile<int>(
+                          title: Text('男'),
+                          value: 1,
+                          groupValue: _selectedSex,
+                          onChanged: (value) {
+                            setState(() => _selectedSex = value!);
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: RadioListTile<int>(
+                          title: Text('女'),
+                          value: 2,
+                          groupValue: _selectedSex,
+                          onChanged: (value) {
+                            setState(() => _selectedSex = value!);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // 保存按钮
+          SizedBox(height: 30),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+              onPressed: _saveChanges,
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                elevation: 5,
+                backgroundColor: Theme.of(context).primaryColor,
+                foregroundColor: Colors.white,
+              ),
+              child: ShaderMask(
+                shaderCallback: (Rect bounds) {
+                  return LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Colors.white,
+                      Colors.white70,
+                    ],
+                  ).createShader(bounds);
+                },
+                child: Text(
+                  '保存修改',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+}
