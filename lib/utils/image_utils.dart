218876995a3bc04d29371b65fc4053dcc90aa7f7
ikunhuaji <1550309783@qq.com>
2024-11-11 16:36:19 +0800

    添加注释

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageUtils {
-  //获取本地图片
+  //获取本地图片 需要异步 对于return值 _pickedFile 的显示方法： FileImage(File(_pickedFile!.path))
   static Future<XFile?> getImage() async {
     return await ImagePicker().pickImage(source: ImageSource.gallery);
   }
