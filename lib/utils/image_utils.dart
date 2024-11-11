9633e9b248bd5823aa4dc88c41bb1b455f2b2fcb
ikunhuaji <1550309783@qq.com>
2024-11-11 16:33:07 +0800

    图片工具类可获取本地图片和把图片转为FormData

---
+++ lib/utils/image_utils.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageUtils {
+  //获取本地图片
+  static Future<XFile?> getImage() async {
+    return await ImagePicker().pickImage(source: ImageSource.gallery);
+  }
+
+  //图片传输时转FromData
+  static Future<FormData> toFromData(XFile image) async {
+    return FormData.fromMap({"file": await MultipartFile.fromFile(image.path)});
+  }
+}
