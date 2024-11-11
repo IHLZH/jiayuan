2dcf152552bfd264ce385d417cdbcb0ad68f63bb
ikunhuaji <1550309783@qq.com>
2024-11-11 17:14:13 +0800

    Revert "添加获取相册权限"
    This reverts commit e75349dbc2c7324c48e63f071516ed9adf535179.

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -1,17 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class ImageUtils {
-  // 请求相册权限
-  Future<bool> requestGalleryPermission() async {
-    PermissionStatus status = await Permission.photos.request();
-    if (status.isGranted) {
-      return true;
-    } else {}
-    return false;
-  }
-
   //获取本地图片 需要异步 对于return值 _pickedFile 的显示方法： FileImage(File(_pickedFile!.path))
   static Future<XFile?> getImage() async {
     return await ImagePicker().pickImage(source: ImageSource.gallery);
