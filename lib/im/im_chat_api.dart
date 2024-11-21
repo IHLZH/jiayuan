faa6e1db275d3ba2815837081cc07f7dabee9e57
ikunhuaji <1550309783@qq.com>
2024-11-21 10:02:51 +0800

    添加会话拉取

--- lib/im/im_chat_api.dart
+++ lib/im/im_chat_api.dart
@@ -6,6 +6,8 @@ import 'package:tencent_cloud_chat_sdk/enum/V2TimSDKListener.dart';
 import 'package:tencent_cloud_chat_sdk/enum/log_level_enum.dart';
 import 'package:tencent_cloud_chat_sdk/enum/message_elem_type.dart';
 import 'package:tencent_cloud_chat_sdk/models/v2_tim_callback.dart';
+import 'package:tencent_cloud_chat_sdk/models/v2_tim_conversation.dart';
+import 'package:tencent_cloud_chat_sdk/models/v2_tim_conversation_result.dart';
 import 'package:tencent_cloud_chat_sdk/models/v2_tim_friend_application.dart';
 import 'package:tencent_cloud_chat_sdk/models/v2_tim_friend_info.dart';
 import 'package:tencent_cloud_chat_sdk/models/v2_tim_message.dart';
@@ -468,8 +470,107 @@ class ImChatApi {
     }
   }
 
-  //获取文本消息
-  // Future<void> getTextMessage() async{
-  //   advancedMsgListener.re
-  // }
+  //分页拉取会话
+  Future<List<V2TimConversation?>> getConversationList(
+      String index, int pageSize) async {
+    //获取会话列表
+    V2TimValueCallback<V2TimConversationResult> getConversationListRes =
+        await TencentImSDKPlugin.v2TIMManager
+            .getConversationManager()
+            .getConversationList(
+                count: pageSize, //分页拉取的个数，一次分页拉取不宜太多，会影响拉取的速度，建议每次拉取 100 个会话
+                nextSeq: index //分页拉取的游标，第一次默认取传 0，后续分页拉传上一次分页拉取成功回调里的 nextSeq
+                );
+    if (getConversationListRes.code == 0) {
+      //拉取成功
+      bool? isFinished = getConversationListRes.data?.isFinished; //是否拉取完
+      String? nextSeq = getConversationListRes.data?.nextSeq; //后续分页拉取的游标
+      List<V2TimConversation?>? conversationList =
+          getConversationListRes.data?.conversationList; //此次拉取到的消息列表
+      //如果没有拉取完，使用返回的nextSeq继续拉取直到isFinished为true
+      if (!isFinished!) {
+        V2TimValueCallback<V2TimConversationResult> nextConversationListRes =
+            await TencentImSDKPlugin.v2TIMManager
+                .getConversationManager()
+                .getConversationList(
+                    count: pageSize,
+                    nextSeq: nextSeq =
+                        index); //使用返回的nextSeq继续拉取直到isFinished为true
+      }
+
+      if (isProduction) print("=========== 拉取会话列表成功 ===========");
+
+      getConversationListRes.data?.conversationList?.forEach((element) {
+        element
+            ?.conversationID; //会话唯一 ID，如果是单聊，组成方式为 c2c_userID；如果是群聊，组成方式为 group_groupID。
+        element?.draftText; //草稿信息
+        element?.draftTimestamp; //草稿编辑时间，草稿设置的时候自动生成。
+        element?.faceUrl; //会话展示头像，群聊头像：群头像；单聊头像：对方头像。
+        element?.groupAtInfoList; //群会话 @ 信息列表，通常用于展示 “有人@我” 或 “@所有人” 这两种提醒状态。
+        element?.groupID; //当前群聊 ID，如果会话类型为群聊，groupID 会存储当前群的群 ID，否则为 null。
+        element?.groupType; //当前群聊类型，如果会话类型为群聊，groupType 为当前群类型，否则为 null。
+        element?.isPinned; //会话是否置顶
+        element?.lastMessage; //会话最后一条消息
+        element?.orderkey; //会话排序字段
+        element?.recvOpt; //消息接收选项
+        element
+            ?.showName; //会话展示名称，群聊会话名称优先级：群名称 > 群 ID；单聊会话名称优先级：对方好友备注 > 对方昵称 > 对方的 userID。
+        element?.type; //会话类型，分为 C2C（单聊）和 Group（群聊）。
+        element?.unreadCount; //会话未读消息数，直播群（AVChatRoom）不支持未读计数，默认为 0。
+        element?.userID; //对方用户 ID，如果会话类型为单聊，userID 会存储对方的用户 ID，否则为 null。
+      });
+
+      return getConversationListRes.data?.conversationList ?? [];
+    } else {
+      if (isProduction) print("=========== 拉取会话列表失败 ===========");
+      if (isProduction)
+        print(
+            "错误码：${getConversationListRes.code} 错误信息： ${getConversationListRes.desc}");
+      return [];
+    }
+  }
+
+  //获取指定单个会话
+  Future<V2TimConversation?> getConversation(String conversationID) async {
+    //获取指定会话
+    V2TimValueCallback<V2TimConversation> getConversationtRes =
+        await TencentImSDKPlugin.v2TIMManager
+            .getConversationManager()
+            .getConversation(
+                conversationID:
+                    conversationID); //会话唯一 ID，如果是 C2C 单聊，组成方式为 c2c_userID，如果是群聊，组成方式为 group_groupID
+    if (getConversationtRes.code == 0) {
+      //拉取成功
+      getConversationtRes.data
+          ?.conversationID; //会话唯一 ID，如果是单聊，组成方式为 c2c_userID；如果是群聊，组成方式为 group_groupID。
+      getConversationtRes.data?.draftText; //草稿信息
+      getConversationtRes.data?.draftTimestamp; //草稿编辑时间，草稿设置的时候自动生成。
+      getConversationtRes.data?.faceUrl; //会话展示头像，群聊头像：群头像；单聊头像：对方头像。
+      getConversationtRes
+          .data?.groupAtInfoList; //群会话 @ 信息列表，通常用于展示 “有人@我” 或 “@所有人” 这两种提醒状态。
+      getConversationtRes
+          .data?.groupID; //当前群聊 ID，如果会话类型为群聊，groupID 会存储当前群的群 ID，否则为 null。
+      getConversationtRes
+          .data?.groupType; //当前群聊类型，如果会话类型为群聊，groupType 为当前群类型，否则为 null。
+      getConversationtRes.data?.isPinned; //会话是否置顶
+      getConversationtRes.data?.lastMessage; //会话最后一条消息
+      getConversationtRes.data?.orderkey; //会话排序字段
+      getConversationtRes.data?.recvOpt; //消息接收选项
+      getConversationtRes.data
+          ?.showName; //会话展示名称，群聊会话名称优先级：群名称 > 群 ID；单聊会话名称优先级：对方好友备注 > 对方昵称 > 对方的 userID。
+      getConversationtRes.data?.type; //会话类型，分为 C2C（单聊）和 Group（群聊）。
+      getConversationtRes
+          .data?.unreadCount; //会话未读消息数，直播群（AVChatRoom）不支持未读计数，默认为 0。
+      getConversationtRes
+          .data?.userID; //对方用户 ID，如果会话类型为单聊，userID 会存储对方的用户 ID，否则为 null。
+
+      return getConversationtRes.data;
+    } else {
+      if (isProduction) print("============= 获取会话失败 ============");
+      if (isProduction)
+        print(
+            "错误码：${getConversationtRes.code} 错误信息： ${getConversationtRes.desc}");
+      return null;
+    }
+  }
 }
