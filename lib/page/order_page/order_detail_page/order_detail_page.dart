25cf85f9f63323d396ca80bb2bb9c14101681316
ikunhuaji <1550309783@qq.com>
2024-11-25 21:58:30 +0800

    添加待支付状态不验收按钮

--- lib/page/order_page/order_detail_page/order_detail_page.dart
+++ lib/page/order_page/order_detail_page/order_detail_page.dart
@@ -638,8 +638,19 @@ class _OrderDetailPageState extends State<OrderDetailPage> {
                       ],
                     ),
                   ),
-                4 => _buildIconButton(
-                    Icons.payment, '去支付', AppColors.orangeBtnColor),
+                4 => Container(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      _buildIconButton(
+                          Icons.payment, '去支付', AppColors.orangeBtnColor),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      _buildIconButton(Icons.cancel, '不验收', Colors.red),
+                    ],
+                  ),
+                ),
                 5 => _buildIconButton(
                     Icons.rate_review_outlined, '去评价', AppColors.appColor),
                 _ => Container(),
