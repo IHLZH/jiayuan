914c044026f7b069859a7edd302c119025cc8a57
ywl <3101653644@qq.com>
2024-12-17 08:40:28 +0800

    增加收益分析的功能

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:jiayuan/app.dart';
 import 'package:jiayuan/http/url_path.dart';
 import 'package:jiayuan/page/ai_customer_service_page/ai_customer_service_vm.dart';
-import 'package:jiayuan/page/chat_page/conversation_page_vm.dart';
 import 'package:jiayuan/page/tab_page/tab_page.dart';
 import 'package:provider/provider.dart';
 
 import 'http/dio_instance.dart';
 
 Future<void> main() async {
-  DioInstance.instance().initDio(baseUrl: UrlPath.testBaseUrl);
+  DioInstance.instance().initDio(baseUrl: UrlPath.yuwenBaseUrl);
   await ScreenUtil.ensureScreenSize();
   runApp(MultiProvider(
     providers: [
